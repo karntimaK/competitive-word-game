63d299b5e0e3d3fb0be413adf4f83af520db1c5a
nattaphat010447 <jangkup90@gmail.com>
2025-10-02 13:07:06 +0700

    Update main.js
    add
    * connect Socket.IO
    * handle events

--- static/main.js
+++ static/main.js
@@ -2,4 +2,11 @@ const socket = io("http://localhost:5000");
 
 socket.on("connect", () => {
   console.log("Connected to server");
+  document.getElementById("status").innerText = "Connected to server.";
 });
+
+function findMatch() {
+  const username = document.getElementById("username").value || "Anonymous";
+  socket.emit("find_match", { username: username });
+  document.getElementById("status").innerText = "Finding a match...";
+}
